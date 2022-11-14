26f53d161354b8d21f56403d38bea95b49b30ae3
kailasvs <cyberonicskailas@gmail.com>
2022-11-14 12:39:52 +0530

    updated content language feature

--- userdetails/views.py
+++ userdetails/views.py
@@ -120,7 +120,7 @@ class get_content_by_language(APIView):
     @swagger_auto_schema(
         operation_description="Language",
         manual_parameters=[openapi.Parameter(
-            'language_id', 
+            'language_name', 
             openapi.IN_QUERY, 
             type=openapi.TYPE_STRING
             )],
@@ -129,8 +129,8 @@ class get_content_by_language(APIView):
    
         try:
             hostname = f"{ request.scheme }://{ request.get_host() }"
-            lang_id = request.GET.get('language_id') 
-            appts = content.objects.filter(language_id=lang_id).values('id','language','language__name','video','audio','text_content','title',
+            lang_id = request.GET.get('language_name') 
+            appts = content.objects.filter(language__name=lang_id).values('id','language','language__name','video','audio','text_content','title',
                                                                     'speciality','video_link','short_video','short_video_link','html_content','more_info',
                                                                     'meeting_link','language__native_name','language__video_name','language__audio_name',
                                                                     'language__text_name','language__home_name','language__meeting_name','language__reviews_name','short_video_thumbnail_desktop','short_video_thumbnail_mobile','video_thumbnail_mobile','video_thumbnail_desktop')
